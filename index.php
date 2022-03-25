92f3fc98bb9572fea8b1542f88d5e0362861ecb9
TempleLo <templelo5408@gmail.com>
2022-03-25 17:37:11 +0800

    Add first version of image gallery.

--- index.php
+++ index.php
@@ -2,10 +2,23 @@
 $title = 'Wibly Wobly Sounds';
 require_once './includes/header.php';
 ?>
+<link rel="stylesheet" href="./css/image_gallery.css">
 <div id="tsparticles"></div>
-<div class="startTourBtnContainer">
-    <div class="center">
-        <button type="button" class="btn btn-dark btn-lg startTourBtn shakeBtn">Start Tour!</button>
-    </div>
+<div class="image_gallery_wrapper">
+    <img src="https://source.unsplash.com/random/600x600?water" alt="">
+    <img src="https://source.unsplash.com/random/600x600?summer" alt="">
+    <img src="https://source.unsplash.com/random/600x600?plants" alt="">
+    <img src="https://source.unsplash.com/random/600x600?snow" alt="">
+    <img src="https://source.unsplash.com/random/600x600?roses" alt="">
+    <img src="https://source.unsplash.com/random/600x600?sky" alt="">
+    <img src="https://source.unsplash.com/random/600x600?nature" alt="">
+    <img src="https://source.unsplash.com/random/600x600?blossom" alt="">
+    <img src="https://source.unsplash.com/random/600x600?ice" alt="">
+    <img src="https://source.unsplash.com/random/600x600?spring" alt="">
 </div>
+<!--<div class="startTourBtnContainer">-->
+<!--    <div class="center">-->
+<!--        <button type="button" class="btn btn-dark btn-lg startTourBtn shakeBtn">Start Tour!</button>-->
+<!--    </div>-->
+<!--</div>-->
 <?php require_once './includes/footer.php'; ?>
